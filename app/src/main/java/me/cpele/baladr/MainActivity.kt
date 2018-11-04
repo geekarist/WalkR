053e97493ce8c1dbe4ef991951a9fe0ac69b5a0f
Christobal <geekarist@users.noreply.github.com>
2018-11-04 17:50:17 +0100

    Display home button except at start destination

--- app/src/main/java/me/cpele/baladr/MainActivity.kt
+++ app/src/main/java/me/cpele/baladr/MainActivity.kt
@@ -1,12 +1,32 @@
 package me.cpele.baladr
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.findNavController
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        navHostFragment.findNavController().apply {
+            addOnNavigatedListener { controller, destination ->
+                supportActionBar?.apply {
+                    val isAtStart = destination.id == controller.graph.startDestination
+                    setDisplayShowHomeEnabled(!isAtStart)
+                    setDisplayHomeAsUpEnabled(!isAtStart)
+                }
+            }
+        }
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        return when (item?.itemId) {
+            android.R.id.home -> navHostFragment.findNavController().popBackStack()
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 }
