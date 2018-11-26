a111e957cd7ae59de20330d55b6eba139a8c4f74
Christobal <geekarist@users.noreply.github.com>
2018-11-26 08:46:59 +0100

    Simplify constraint to step

--- app/src/main/java/me/cpele/baladr/PlaylistGenerationViewModel.kt
+++ app/src/main/java/me/cpele/baladr/PlaylistGenerationViewModel.kt
@@ -4,7 +4,6 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.Transformations
 import androidx.lifecycle.ViewModel
-import kotlin.math.absoluteValue
 
 class PlaylistGenerationViewModel : ViewModel() {
 
@@ -17,21 +16,11 @@ class PlaylistGenerationViewModel : ViewModel() {
     val tempo: LiveData<Int> = _tempo
 
     fun onProgressChanged(progress: Int) {
-        if (_progress.value != progress) {
-            val range = 0..200 step 10
-            _progress.value = range.closest(progress)
+        _progress.value?.let { value ->
+            if (value != progress) {
+                _progress.value = (progress / 10) * 10
+            }
         }
     }
 }
 
-private fun IntProgression.closest(value: Int): Int? {
-    var closest: Int = Int.MAX_VALUE
-    this.forEach {
-        val distanceToIt = (value - it).absoluteValue
-        val distanceToClosest = (value - closest).absoluteValue
-        if (distanceToIt < distanceToClosest) {
-            closest = it
-        }
-    }
-    return if (closest == Int.MAX_VALUE) null else closest
-}
