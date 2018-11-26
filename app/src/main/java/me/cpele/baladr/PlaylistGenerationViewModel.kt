e39050230de8572536217d3e0eab3f705cc77e22
Christobal <geekarist@users.noreply.github.com>
2018-11-26 08:41:17 +0100

    Constraint tempo to step of 10

--- app/src/main/java/me/cpele/baladr/PlaylistGenerationViewModel.kt
+++ app/src/main/java/me/cpele/baladr/PlaylistGenerationViewModel.kt
@@ -4,6 +4,7 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.Transformations
 import androidx.lifecycle.ViewModel
+import kotlin.math.absoluteValue
 
 class PlaylistGenerationViewModel : ViewModel() {
 
@@ -11,13 +12,26 @@ class PlaylistGenerationViewModel : ViewModel() {
     val progress: LiveData<Int> = _progress
 
     private val _tempo = Transformations.map(progress) {
-        it + 60
+        it + 70
     }
     val tempo: LiveData<Int> = _tempo
 
     fun onProgressChanged(progress: Int) {
         if (_progress.value != progress) {
-            _progress.value = progress
+            val range = 0..200 step 10
+            _progress.value = range.closest(progress)
         }
     }
+}
+
+private fun IntProgression.closest(value: Int): Int? {
+    var closest: Int = Int.MAX_VALUE
+    this.forEach {
+        val distanceToIt = (value - it).absoluteValue
+        val distanceToClosest = (value - closest).absoluteValue
+        if (distanceToIt < distanceToClosest) {
+            closest = it
+        }
+    }
+    return if (closest == Int.MAX_VALUE) null else closest
 }
