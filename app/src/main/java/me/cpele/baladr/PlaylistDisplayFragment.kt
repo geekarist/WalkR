4b8d08537bcfdf6784cf9d85811c56fa0827e92f
Christobal <geekarist@users.noreply.github.com>
2018-12-01 10:34:20 +0100

    Use Toast instead of Snackbar

--- app/src/main/java/me/cpele/baladr/PlaylistDisplayFragment.kt
+++ app/src/main/java/me/cpele/baladr/PlaylistDisplayFragment.kt
@@ -8,12 +8,12 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import androidx.navigation.fragment.findNavController
-import com.google.android.material.snackbar.Snackbar
 import kotlinx.android.synthetic.main.fragment_playlist_display.*
 
 class PlaylistDisplayFragment : Fragment() {
@@ -83,11 +83,12 @@ class PlaylistDisplayFragment : Fragment() {
 
         viewModel.playlistSaveEvent.observe(this, Observer {
             it.consumed?.apply {
-                Snackbar.make(
-                    view,
-                    "Playlist '${name}' has been saved with ${trackIds.size} tracks",
-                    Snackbar.LENGTH_LONG
+                Toast.makeText(
+                    context,
+                    "Playlist '$name' has been saved with ${trackIds.size} tracks",
+                    Toast.LENGTH_LONG
                 ).show()
+
                 findNavController().navigate(R.id.action_playlistDisplayFragment_to_libraryFragment)
             }
         })
