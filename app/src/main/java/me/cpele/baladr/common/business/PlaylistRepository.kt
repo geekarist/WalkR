2fe555db66768e19c306054ce032dcdb7a4a7517
Christobal <geekarist@users.noreply.github.com>
2018-12-19 09:02:59 +0100

    Extract insert* functions, use `with`

--- app/src/main/java/me/cpele/baladr/common/business/PlaylistRepository.kt
+++ app/src/main/java/me/cpele/baladr/common/business/PlaylistRepository.kt
@@ -3,6 +3,9 @@ package me.cpele.baladr.common.business
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import com.github.kittinunf.fuel.Fuel
+import com.github.kittinunf.fuel.core.FuelError
+import com.github.kittinunf.fuel.core.Request
+import com.github.kittinunf.fuel.core.Response
 import com.google.gson.Gson
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
@@ -22,19 +25,23 @@ class PlaylistRepository(
             joinResult
                 ?.groupBy { it: JoinPlaylistTrackWrapper -> it.playlist }
                 ?.map { entry: Map.Entry<PlaylistEntity, List<JoinPlaylistTrackWrapper>> ->
-                    PlaylistBo(
-                        entry.key.plId,
-                        entry.key.plName,
-                        entry.value.map { groupedJoinResult: JoinPlaylistTrackWrapper ->
-                            TrackBo(
-                                groupedJoinResult.track.trId,
-                                groupedJoinResult.track.trCover,
-                                groupedJoinResult.track.trTitle,
-                                groupedJoinResult.track.trArtist,
-                                groupedJoinResult.track.trDuration,
-                                groupedJoinResult.track.trTempo
-                            )
-                        })
+                    with(entry) {
+                        PlaylistBo(
+                            key.plId,
+                            key.plName,
+                            value.map { groupedJoinResult: JoinPlaylistTrackWrapper ->
+                                with(groupedJoinResult.track) {
+                                    TrackBo(
+                                        trId,
+                                        trCover,
+                                        trTitle,
+                                        trArtist,
+                                        trDuration,
+                                        trTempo
+                                    )
+                                }
+                            })
+                    }
                 }
         }
     }
@@ -46,24 +53,9 @@ class PlaylistRepository(
         GlobalScope.launch {
 
             try {
-                val playlistEntity = PlaylistEntity(plName = playlist.name)
-                val insertedPlaylistId = playlistDao.insert(playlistEntity)
+                insertEntities(playlist)
 
-                // We know that playlist.tracks come from the db so we don't need to insert them
-                val playlistTrackEntities = playlist.tracks.map {
-                    PlaylistTrackEntity(insertedPlaylistId, it.id)
-                }
-                playlistTrackDao.insertAll(playlistTrackEntities)
-
-                val (_, _, result) = Fuel.post("https://api.spotify.com/v1/me/playlists")
-                    .header(
-                        mapOf(
-                            "Authorization" to "Bearer BQBS2oOuyDowgtNCB2yQoNLM1_XzOoTNiByI00hDCT1Hrrxy7Ye2dIVo786sD0xLXSoVNnt-AXgbgBjvahhl7lNlVebG0Bly64wkgg5GrIDRX7r3FFOPN4c2fHTfqPTt47epB3GAM_KtJDskOciOkP6CdnSREK14bD8VeWkC4hp2G9M4Nl5G5yvprWtRLr6_oXfXRNIg8M9h1mwd9igRYisprJE2Hw",
-                            "Content-Type" to "application/json"
-                        )
-                    ).body(gson.toJson(PlaylistDto(name = playlist.name)))
-                    .response()
-
+                val (_, _, result) = insertResource(playlist)
                 resultData.postValue(
                     when (result) {
                         is com.github.kittinunf.result.Result.Success -> Result.success(Unit)
@@ -75,5 +67,27 @@ class PlaylistRepository(
             }
         }
         return resultData
+    }
+
+    private fun insertResource(playlist: PlaylistBo): Triple<Request, Response, com.github.kittinunf.result.Result<ByteArray, FuelError>> {
+        return Fuel.post("https://api.spotify.com/v1/me/playlists")
+            .header(
+                mapOf(
+                    "Authorization" to "Bearer BQBS2oOuyDowgtNCB2yQoNLM1_XzOoTNiByI00hDCT1Hrrxy7Ye2dIVo786sD0xLXSoVNnt-AXgbgBjvahhl7lNlVebG0Bly64wkgg5GrIDRX7r3FFOPN4c2fHTfqPTt47epB3GAM_KtJDskOciOkP6CdnSREK14bD8VeWkC4hp2G9M4Nl5G5yvprWtRLr6_oXfXRNIg8M9h1mwd9igRYisprJE2Hw",
+                    "Content-Type" to "application/json"
+                )
+            ).body(gson.toJson(PlaylistDto(name = playlist.name)))
+            .response()
+    }
+
+    private fun insertEntities(playlist: PlaylistBo) {
+        val playlistEntity = PlaylistEntity(plName = playlist.name)
+        val insertedPlaylistId = playlistDao.insert(playlistEntity)
+
+        // We know that playlist.tracks come from the db so we don't need to insert them
+        val playlistTrackEntities = playlist.tracks.map {
+            PlaylistTrackEntity(insertedPlaylistId, it.id)
+        }
+        playlistTrackDao.insertAll(playlistTrackEntities)
     }
 }
