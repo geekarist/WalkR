aaf2d9f5b4f28f7c57c7c072b5422fb24692961e
Christobal <geekarist@users.noreply.github.com>
2019-01-16 08:57:04 +0100

    Use SensorManager to detect tempo

--- app/src/main/java/me/cpele/baladr/feature/playlistgeneration/PlaylistGenerationViewModel.kt
+++ app/src/main/java/me/cpele/baladr/feature/playlistgeneration/PlaylistGenerationViewModel.kt
@@ -1,14 +1,22 @@
 package me.cpele.baladr.feature.playlistgeneration
 
+import android.app.Application
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
+import android.os.SystemClock
+import android.widget.Toast
+import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.Transformations
-import androidx.lifecycle.ViewModel
 import kotlinx.coroutines.*
+import java.util.*
 import java.util.concurrent.TimeUnit
 import kotlin.coroutines.CoroutineContext
 
-class PlaylistGenerationViewModel : ViewModel(), CoroutineScope {
+class PlaylistGenerationViewModel(private val app: Application) : AndroidViewModel(app), CoroutineScope {
 
     private val job = Job()
     override val coroutineContext: CoroutineContext = Dispatchers.Default + job
@@ -25,6 +33,10 @@ class PlaylistGenerationViewModel : ViewModel(), CoroutineScope {
     val tempoDetectButtonEnabled: LiveData<Boolean> = Transformations.map(_detectionRunning) { !it }
     val seekBarEnabled: LiveData<Boolean> = Transformations.map(_detectionRunning) { !it }
 
+    private val sensorManager: SensorManager by lazy {
+        app.getSystemService(Application.SENSOR_SERVICE) as SensorManager
+    }
+
     fun onProgressChanged(progress: Int) {
         _progress.value?.let { value ->
             if (value != progress) {
@@ -33,16 +45,51 @@ class PlaylistGenerationViewModel : ViewModel(), CoroutineScope {
         }
     }
 
+    private var listener: StepCountSensorListener? = null
+
     fun onStartTempoDetection(durationSeconds: Int) = launch {
+        val endTimeMsec = Date().time + TimeUnit.SECONDS.toMillis(durationSeconds.toLong())
+        listener?.let(sensorManager::unregisterListener)
+        listener = StepCountSensorListener(endTimeMsec)
+        val defaultSensor = sensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER)
+        sensorManager.registerListener(
+            listener,
+            defaultSensor,
+            SensorManager.SENSOR_DELAY_FASTEST
+        )
         _detectionRunning.postValue(true)
-        delay(TimeUnit.SECONDS.toMillis(durationSeconds.toLong()))
+        delay(TimeUnit.SECONDS.toMillis(11))
         _detectionRunning.postValue(false)
-        val count = 15
-        val tempo = count * (60 / durationSeconds)
-        _progress.postValue(tempo - 70)
+    }
+
+    inner class StepCountSensorListener(private val endTimeMsec: Long) : SensorEventListener {
+        override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) = Unit
+
+        override fun onSensorChanged(event: SensorEvent?) {
+            event?.values?.get(0)?.let { count ->
+                val timestampMsec =
+                    System.currentTimeMillis() -
+                            SystemClock.elapsedRealtime() +
+                            TimeUnit.NANOSECONDS.toMillis(event.timestamp)
+                if (timestampMsec >= endTimeMsec) {
+                    sensorManager.unregisterListener(this)
+                    Toast.makeText(
+                        app,
+                        "timestamp: ${Date(event.timestamp)}, endTimeMsec: ${Date(endTimeMsec)}",
+                        Toast.LENGTH_LONG
+                    ).show()
+                    val diffMsec = timestampMsec - endTimeMsec
+                    val diffMin = TimeUnit.MILLISECONDS.toMinutes(diffMsec)
+                    val countPerMin = count / diffMin
+                    _progress.postValue(countPerMin.toInt() - 70)
+                    _detectionRunning.postValue(false)
+                }
+            }
+        }
     }
 
     override fun onCleared() {
+        listener?.let(sensorManager::unregisterListener)
         job.cancel()
         super.onCleared()
     }
