a189c2803668ad469fa3f7b81d9d36c668e59e42
Christobal <geekarist@users.noreply.github.com>
2019-03-01 08:41:05 +0100

    [#15] Fix tap tempo algorithm my way

--- app/src/main/java/me/cpele/baladr/feature/playlistgeneration/TapTempoViewModel.kt
+++ app/src/main/java/me/cpele/baladr/feature/playlistgeneration/TapTempoViewModel.kt
@@ -2,45 +2,50 @@ package me.cpele.baladr.feature.playlistgeneration
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Transformations
 import androidx.lifecycle.ViewModel
 import java.util.*
 import java.util.concurrent.TimeUnit
+import kotlin.math.roundToInt
 
 class TapTempoViewModel : ViewModel() {
 
-    private val _beatsPerMin = MutableLiveData<Int>()
+    private val tapsData = MutableLiveData<MutableList<Long>>().apply { value = mutableListOf() }
+
+    private val _beatsPerMin: LiveData<Int> = Transformations.map(tapsData) { nullableTaps: MutableList<Long>? ->
+        nullableTaps
+            ?.takeIf { it.size >= 2 }
+            ?.let { taps ->
+                val firstTapMsec = taps.min() ?: return@let null
+                val lastTapMsec = taps.max() ?: return@let null
+                val diffMsec = lastTapMsec - firstTapMsec
+                val beatsPerMsec = taps.size.dec().toDouble() / diffMsec
+                (beatsPerMsec * TimeUnit.MINUTES.toMillis(1)).roundToInt()
+            } ?: 0
+    }
+
     val beatsPerMin: LiveData<Int> = _beatsPerMin
 
-    private var previousTapMsec: Long = 0
-    private var firstTapMsec: Long = 0
-    private var tapCount: Int = 0
-
     fun onTap() {
-        val lastTapMsec = Date().time
-        val isResetDelayElapsed = lastTapMsec - previousTapMsec > TimeUnit.SECONDS.toMillis(2)
-        if (isResetDelayElapsed) {
-            tapCount = 0
+        val nowMsec = Date().time
+        val tapsList = tapsData.value
+        if (isResetDelayElapsed(tapsList)) {
+            tapsList?.clear()
         }
+        tapsList?.add(nowMsec)
+        tapsData.value = tapsList?.toMutableList()
+    }
 
-        if (tapCount == 0) {
-            firstTapMsec = lastTapMsec
-            tapCount = 1
-        } else {
-            val timeSinceFirstTapMsec = lastTapMsec - firstTapMsec
-            val beatsPerMsec = tapCount.toDouble() / timeSinceFirstTapMsec
-            val beatsPerMin = 60_000 * beatsPerMsec
-            _beatsPerMin.value = beatsPerMin.toInt()
-            tapCount++
-        }
-
-        previousTapMsec = lastTapMsec
+    private fun isResetDelayElapsed(tapsList: MutableList<Long>?): Boolean {
+        val nowMsec = Date().time
+        val lastTapMsec = tapsList?.max() ?: return false
+        return nowMsec - lastTapMsec > TimeUnit.SECONDS.toMillis(2)
     }
 
 
     fun onReset() {
-        _beatsPerMin.value = 0
-        previousTapMsec = 0
-        firstTapMsec = 0
-        tapCount = 0
+        val tapsList = tapsData.value
+        tapsList?.clear()
+        tapsData.value = tapsList
     }
 }
