51bfc47642099861c96a4230306d45f842c8a6fd
Christobal <geekarist@users.noreply.github.com>
2019-03-05 07:42:46 +0100

    [#21] Extract TapTempoMeasurement

--- app/src/main/java/me/cpele/baladr/feature/playlistgeneration/TapTempoViewModel.kt
+++ app/src/main/java/me/cpele/baladr/feature/playlistgeneration/TapTempoViewModel.kt
@@ -1,51 +1,10 @@
 package me.cpele.baladr.feature.playlistgeneration
 
 import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.Transformations
 import androidx.lifecycle.ViewModel
-import java.util.*
-import java.util.concurrent.TimeUnit
-import kotlin.math.roundToInt
 
-class TapTempoViewModel : ViewModel() {
-
-    private val tapsData = MutableLiveData<MutableList<Long>>().apply { value = mutableListOf() }
-
-    private val _beatsPerMin: LiveData<Int> = Transformations.map(tapsData) { nullableTaps: MutableList<Long>? ->
-        nullableTaps
-            ?.takeIf { it.size >= 2 }
-            ?.let { taps ->
-                val firstTapMsec = taps.min() ?: return@let null
-                val lastTapMsec = taps.max() ?: return@let null
-                val diffMsec = lastTapMsec - firstTapMsec
-                val beatsPerMsec = taps.size.dec().toDouble() / diffMsec
-                (beatsPerMsec * TimeUnit.MINUTES.toMillis(1)).roundToInt()
-            } ?: 0
-    }
-
-    val beatsPerMin: LiveData<Int> = _beatsPerMin
-
-    fun onTap() {
-        val nowMsec = Date().time
-        val tapsList = tapsData.value
-        if (isResetDelayElapsed(tapsList)) {
-            tapsList?.clear()
-        }
-        tapsList?.add(nowMsec)
-        tapsData.value = tapsList?.toMutableList()
-    }
-
-    private fun isResetDelayElapsed(tapsList: MutableList<Long>?): Boolean {
-        val nowMsec = Date().time
-        val lastTapMsec = tapsList?.max() ?: return false
-        return nowMsec - lastTapMsec > TimeUnit.SECONDS.toMillis(2)
-    }
-
-
-    fun onReset() {
-        val tapsList = tapsData.value
-        tapsList?.clear()
-        tapsData.value = tapsList
-    }
+class TapTempoViewModel(private val measurement: TapTempoMeasurement) : ViewModel() {
+    val beatsPerMin: LiveData<Int> = measurement.beatsPerMin
+    fun onTap() = measurement.onTap()
+    fun onReset() = measurement.onReset()
 }
