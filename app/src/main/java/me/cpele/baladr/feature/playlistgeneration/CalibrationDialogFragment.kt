18119f37dd31e274774b1e640c7900647cacace2
Christobal <geekarist@users.noreply.github.com>
2019-03-05 07:58:29 +0100

    [#21] Inflate fragment_tap_tempo

--- app/src/main/java/me/cpele/baladr/feature/playlistgeneration/CalibrationDialogFragment.kt
+++ app/src/main/java/me/cpele/baladr/feature/playlistgeneration/CalibrationDialogFragment.kt
@@ -1,10 +1,19 @@
 package me.cpele.baladr.feature.playlistgeneration
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.DialogFragment
+import me.cpele.baladr.R
 
 class CalibrationDialogFragment : DialogFragment() {
 
     companion object {
         fun newInstance() = CalibrationDialogFragment()
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_tap_tempo, container, false)
     }
 }
