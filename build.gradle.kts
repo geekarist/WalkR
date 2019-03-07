d1b6edbe95ef87a0d053504994466557f41e0f0c
Christobal <geekarist@users.noreply.github.com>
2019-03-07 08:41:32 +0100

    Upgrade gradle plugin, remove `clean` val

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,7 +8,7 @@ buildscript {
 
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:3.4.0-beta03")
+        classpath("com.android.tools.build:gradle:3.4.0-beta05")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlin_version")
         // NOTE: Do not place your application dependencies here; they belong
         // in the individual module build.gradle files
@@ -22,11 +22,5 @@ allprojects {
         mavenLocal()
         maven { setUrl("https://jitpack.io") }
         maven { setUrl("https://dl.bintray.com/musichin/maven") }
-    }
-}
-
-tasks {
-    val clean by registering(Delete::class) {
-        delete(buildDir)
     }
 }
